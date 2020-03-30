b9a69ce5f635ec0634bf95e9dd7a9ed06ea12cbe
Anton Smorodskyi <asmorodskyi@suse.com>
2020-03-30 17:06:14 +0200

    process Variables page

--- pageobjects.py
+++ pageobjects.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 
 class PageObject:
@@ -47,3 +48,63 @@ class Cluster(PageObject):
         self.driver.execute_script(
             "arguments[0].scrollIntoView(true);", btn_variables)
         btn_variables.click()
+
+
+class Variables(PageObject):
+
+    input_xpath_template = '//input[@name="variables[{}]"]'
+
+    def __init__(self, driver, values, cluster_labels):
+        super().__init__(driver)
+        self.values = values
+        self.cluster_labels = cluster_labels
+
+    def __insert_value_for(self, key):
+        element = self.get_element(
+            Variables.input_xpath_template.format(key))
+        element.send_keys(self.values[key])
+
+    def __insert_public_key(self):
+        with open(self.values['ssh_public_key'], 'r') as file:
+            key = file.read()
+            file.close()
+            element = self.get_element(
+                Variables.input_xpath_template.format('ssh_public_key'))
+            element.send_keys(key)
+
+    def __insert_cluster_labels(self):
+        for key, value in self.cluster_labels:
+            key_input = self.get_element(
+                '//input[@id="cluster_labels_new_key"]')
+            value_input = self.get_element(
+                '//input[@id="cluster_labels_new_value"]')
+            key_input.send_keys(key)
+            key_input.send_keys(value)
+            add_btn = self.get_element(
+                '//button[@id="cluster_labels_add_map"]')
+            self.driver.execute_script(
+                "arguments[0].scrollIntoView(true);", add_btn)
+            add_btn.click()
+            time.sleep(1)
+
+    def insert_data(self):
+        self.__insert_value_for("subscription_id")
+        self.__insert_value_for("resource_group")
+        self.__insert_value_for("location")
+        self.__insert_value_for("client_id")
+        self.__insert_value_for("client_secret")
+        self.__insert_value_for("tenant_id")
+        self.__insert_value_for("ssh_username")
+        self.__insert_public_key()
+        self.__insert_value_for("cluster_admin_password")
+        self.__insert_value_for("uaa_admin_client_secret")
+        self.__insert_cluster_labels()
+        self.__insert_value_for("dns_zone_name")
+        self.__insert_value_for("cap_domain")
+        self.__insert_value_for("email")
+
+    def go_to_plan(self):
+        btn_plan = self.get_element('//a[@href="/plan"]')
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView(true);", btn_plan)
+        btn_plan.click()
--- test_aks.py
+++ test_aks.py
@@ -3,18 +3,43 @@ import configparser
 from pageobjects import WelcomePage
 from pageobjects import SideBar
 from pageobjects import Cluster
+from pageobjects import Variables
 import pytest
 
 
-def test_simpleFlow():
+@pytest.fixture
+def variables_values():
     config = configparser.RawConfigParser()
     config.read('test.properties')
+    keys = ["subscription_id", "resource_group", "location", "client_id", "client_secret", "tenant_id",
+            "ssh_username", "ssh_public_key", "cluster_admin_password", "uaa_admin_client_secret", "dns_zone_name", "cap_domain", "email"]
+    return dict(map(lambda x: (x, config.get('variables', x)), keys))
+
+
+@pytest.fixture
+def login():
+    config = configparser.RawConfigParser()
+    config.read('test.properties')
+    return {"username": config.get('login', 'username'), "pw": config.get('login', 'pw'), "ip": config.get('login', 'ip')}
+
+
+@pytest.fixture
+def cluster_labels():
+    config = configparser.RawConfigParser()
+    config.read('test.properties')
+    return config.items('cluster_labels')
+
+
+def test_simpleFlow(login, variables_values, cluster_labels):
     driver = webdriver.Firefox(service_log_path='/tmp/geckodriver.log')
-    driver.get("http://{}:{}@{}/".format(config.get('aks', 'username'),
-                                         config.get('aks', 'pw'), config.get('aks', 'ip')))
+    driver.get(
+        "http://{}:{}@{}/".format(login["username"], login["pw"], login["ip"]))
     driver.maximize_window()
     SideBar(driver).page_displayed()
     WelcomePage(driver).go_to_cluster()
     cluster = Cluster(driver)
     cluster.page_displayed()
     cluster.go_to_variables()
+    variables = Variables(driver, variables_values, cluster_labels)
+    variables.insert_data()
+    variables.go_to_plan()
